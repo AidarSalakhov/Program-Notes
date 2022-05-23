087bfc0ea278f0cd1bce31b38abc49f35e01fe6f
Aidar Salakhov <adr-sal@yandex.ru>
2022-05-23 14:18:01 +0300

    Добавьте файлы проекта.

---
+++ Program Notes/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_Notes
+{
+    internal class Program
+    {
+        //Программа “Заметки”
+        //Программа хранит список заметок, пользователь может добавлять, редактировать и удалять заметки.
+
+        //Функционал:
+
+        //•Вывод всех заметок
+        //•Добавление новой заметки в список
+        //•Удаление существующей заметки из списка
+        //•Удаление всех заметок.
+        //•Изменение существующей заметки
+        //•(Дополнительно) сохранение заметок в файл и загрузка
+
+        static void PrintNotes(string[] notes)
+        {
+            for (int i = 0; i < notes.Length; i++)
+            {
+                Console.WriteLine(i + ". " + notes[i]);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string[] notes = { "Пожрать", "Поспасть", "Посрать", "Посмотреть", "Поиграть", "Попинать", "Погулять", "Поугарать", "Попрактиковаться", "Поинтересоваться" };
+
+
+
+
+
+
+
+                Console.WriteLine("Выберите действие");
+                Console.WriteLine("Нажмите A - Посмотреть все заметки");
+                Console.WriteLine("Нажмите Q - Завершить работу программы");
+
+
+
+                switch (Console.ReadKey())
+                {
+                    case ConsoleKey.Q:
+                        break;
+
+                    case "a":
+                        PrintNotes(notes);
+
+                    default:
+                        break;
+                }
+
+
+            }
+        }
+    }
+}
